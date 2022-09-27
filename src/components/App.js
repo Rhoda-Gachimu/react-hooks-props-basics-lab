dc27250a9f666a82ae153d0e68d44ba8386d38fa
Rhoda Gachimu <gachimurhoda@gmail.com>
2022-09-27 20:48:18 +0300

    props

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,28 @@
 import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
-import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
+// import React from "react";
+// import NavBar from "./NavBar";
+// import Home from "./Home";
+import About from "./About"
 import user from "../data/user";
 console.log(user);
+
+
+
+
+// pass this data down as props to the child component(s) that need it!
+
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name="Liza" city="New York" color="firebrick"/>
+      <About bio="I made this" />
+      <App/>
+      
+
     </div>
   );
 }
--- src/data/user.js
+++ src/data/user.js
@@ -1,4 +1,6 @@
+
 const user = {
+  
   name: "Liza",
   city: "New York",
   bio: "I made this!",
